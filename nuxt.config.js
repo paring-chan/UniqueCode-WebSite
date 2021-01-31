a679a011afd1ca649ee9d2d2ffd90462fd77fa09
pikokr <parangee9706@gmail.com>
2021-01-31 20:17:09 +0900

    feat: install tailwindcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/tailwindcss',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
