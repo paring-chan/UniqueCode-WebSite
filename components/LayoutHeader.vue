27a1d34fcf8b2a9edb152998b7d72a3aa49f1f89
pikokr <parangee9706@gmail.com>
2021-02-01 15:19:21 +0900

    fix: menu layout

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -3,8 +3,10 @@
     <div class="flex container h-full mx-auto" style="align-items: center">
       <nuxt-link to="/"><div class="text-xl">UniqueCode</div></nuxt-link>
       <div class="flex-grow" />
-      <div>
+      <div class="flex">
         <menu-item>아이템1</menu-item>
+        <menu-item>아이템2</menu-item>
+        <menu-item>아이템3</menu-item>
       </div>
     </div>
   </div>
