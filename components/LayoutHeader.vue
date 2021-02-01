c749b789ce4d81e89da06db628d52029b477187a
pikokr <parangee9706@gmail.com>
2021-02-01 16:20:40 +0900

    feat: something

--- components/LayoutHeader.vue
+++ components/LayoutHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="border-b h-14 fixed top-0 left-0 w-full bg-white">
     <div class="flex container h-full mx-auto" style="align-items: center">
-      <nuxt-link to="/"><div class="text-xl">UniqueCode</div></nuxt-link>
+      <nuxt-link to="/"
+        ><div class="text-xl pl-4 sm:pl-0">UniqueCode</div></nuxt-link
+      >
       <div class="flex-grow" />
       <div class="flex">
         <menu-item>아이템1</menu-item>
