3b86fd4a5fefaad959a2c29318d5fa2bab0c6173
pikokr <parangee9706@gmail.com>
2021-02-03 20:58:07 +0900

    feat: show more button

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,20 +1,24 @@
 <template>
-  <router-link :to="to">
-    <div class="relative">
-      <img :src="image" alt="strawberryDonut" class="rounded-lg" />
-      <div
-        class="w-full h-full bg-black text-white absolute top-0 left-0 rounded-lg opacity-0 hover:opacity-100 bg-opacity-0 hover:bg-opacity-50 transition-all cursor-pointer flex text-center flex-col"
-        style="align-items: center; justify-content: center"
-      >
-        <div class="text-2xl">
-          {{ title }}
-        </div>
-        <div>
-          {{ description }}
-        </div>
+  <div class="relative">
+    <img :src="image" alt="strawberryDonut" class="rounded-lg" />
+    <div
+      class="w-full h-full bg-black text-white absolute top-0 left-0 rounded-lg opacity-0 hover:opacity-100 bg-opacity-0 hover:bg-opacity-50 transition-all cursor-pointer flex text-center flex-col"
+      style="align-items: center; justify-content: center"
+    >
+      <div class="text-2xl">
+        {{ title }}
       </div>
+      <div>
+        {{ description }}
+      </div>
+      <router-link
+        :to="to"
+        class="border border-white py-2 px-6 rounded-md hover:bg-white hover:text-black transition-colors"
+      >
+        더보기
+      </router-link>
     </div>
-  </router-link>
+  </div>
 </template>
 
 <script>
