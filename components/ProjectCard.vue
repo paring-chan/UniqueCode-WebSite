262b438bc06b3d12c43dbb7860cbd8c0e9b53033
pikokr <parangee9706@gmail.com>
2021-02-05 17:56:31 +0900

    feat: round project card

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="relative">
-    <img :src="image" alt="strawberryDonut" class="rounded-lg" />
+    <img :src="image" alt="strawberryDonut" class="rounded-lg md:rounded-3xl" />
     <div
-      class="w-full h-full bg-black text-white absolute top-0 left-0 rounded-lg opacity-0 hover:opacity-100 bg-opacity-0 hover:bg-opacity-50 transition-all cursor-pointer flex text-center flex-col"
+      class="w-full h-full bg-black text-white absolute top-0 left-0 rounded-lg md:rounded-3xl opacity-0 hover:opacity-100 bg-opacity-0 hover:bg-opacity-50 transition-all cursor-pointer flex text-center flex-col"
       style="align-items: center; justify-content: center"
     >
       <div class="text-2xl">
@@ -13,7 +13,7 @@
       </div>
       <router-link
         :to="to"
-        class="border border-white py-2 px-6 rounded-md hover:bg-white hover:text-black transition-colors"
+        class="border border-white py-2 px-6 rounded-md md:rounded-xl hover:bg-white hover:text-black transition-colors"
       >
         더보기
       </router-link>
