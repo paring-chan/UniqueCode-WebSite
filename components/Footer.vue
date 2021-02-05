5c054a1ac3ee23703ec94214816086992a9a2bb6
pikokr <parangee9706@gmail.com>
2021-02-05 17:03:04 +0900

    feat: 2020 -> 2021

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="flex border-t p-4">
     <div class="flex-grow"></div>
     <div>
-      2020 &copy; UniqueCode | Developed by
+      2021 &copy; UniqueCode | Developed by
       <a href="https://github.com/pikokr" target="_blank" class="text-blue-500"
         >pikokr</a
       >
