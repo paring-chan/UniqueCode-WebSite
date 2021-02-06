f8af34419729d77db767673881031f03b5b39faa
pikokr <parangee9706@gmail.com>
2021-02-06 12:16:37 +0900

    feat: member info overlay

--- components/MemberCard.vue
+++ components/MemberCard.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    <div class="shadow-lg">
-      <img :src="member.avatar" class="rounded-lg" alt="Avatar" />
+    <div class="shadow-lg" @click="open = true">
+      <img
+        :src="member.avatar"
+        class="rounded-lg pointer-events-none"
+        alt="Avatar"
+      />
     </div>
-    <!--    <div class="z-50 fixed bg-black w-full h-full top-0 left-0">test</div>-->
+    <div
+      :class="{
+        'z-50 fixed bg-black w-full h-full overlay': true,
+        active: open,
+      }"
+      @click="open = false"
+    >
+      test
+    </div>
   </div>
 </template>
 
@@ -16,5 +28,23 @@ export default {
       required: true,
     },
   },
+  data: () => ({
+    open: false,
+  }),
 }
 </script>
+
+<style scoped>
+.active {
+  top: 0 !important;
+  opacity: 1 !important;
+  pointer-events: unset !important;
+}
+.overlay {
+  left: 0;
+  top: 100vh;
+  opacity: 0;
+  pointer-events: none;
+  transition: all 0.25s ease-in-out;
+}
+</style>
