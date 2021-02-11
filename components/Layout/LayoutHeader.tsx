6cfe50015abd3681698c64d98a5ab835f158f634
pikokr <parangee9706@gmail.com>
2021-02-11 22:09:29 +0900

    feat: change header component to class

--- components/Layout/LayoutHeader.tsx
+++ components/Layout/LayoutHeader.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const LayoutHeader = () => {
-  return (
-    <div className="bg-white h-16 border-b">
-      <div
-        className="flex px-4 container mx-auto h-full"
-        style={{ alignItems: 'center' }}>
-        <div className="text-xl font-bold">UniqueCode</div>
-        <div className="flex-grow" />
-        <div>메뉴</div>
+class LayoutHeader extends Component<any, any> {
+  render() {
+    return (
+      <div className="bg-white h-16 border-b">
+        <div
+          className="flex px-4 container mx-auto h-full"
+          style={{ alignItems: 'center' }}>
+          <div className="text-xl font-bold">UniqueCode</div>
+          <div className="flex-grow" />
+          <div>메뉴</div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default LayoutHeader
