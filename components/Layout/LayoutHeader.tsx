2117a86108987700968f9499ef3ca11e62fdbed4
pikokr <parangee9706@gmail.com>
2021-02-11 22:04:28 +0900

    feat: basic header

--- components/Layout/LayoutHeader.tsx
+++ components/Layout/LayoutHeader.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const LayoutHeader = () => {
-  return <div className="bg-white h-16 border-b">header</div>
+  return (
+    <div className="bg-white h-16 border-b">
+      <div
+        className="flex px-4 container mx-auto h-full"
+        style={{ alignItems: 'center' }}>
+        <div className="text-xl font-bold">UniqueCode</div>
+        <div className="flex-grow" />
+        <div>메뉴</div>
+      </div>
+    </div>
+  )
 }
 
 export default LayoutHeader
