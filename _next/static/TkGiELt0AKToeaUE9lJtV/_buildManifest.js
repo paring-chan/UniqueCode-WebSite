3ae97e5b18133fedf233f2edf82c4da9aa0fdf22
pikokr <parangee9706@gmail.com>
2021-02-11 11:22:02 +0000

    Deploying to gh-pages from @ pikokr/UniqueCode-WebSite@f9b6e8d3ab4c6b28a0a117968766bbefd78b1b29 🚀

---
+++ _next/static/TkGiELt0AKToeaUE9lJtV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-cc1a006df0ba299a7001.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-81e9d735233ff3fc6a39.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
