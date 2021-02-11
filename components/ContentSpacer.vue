19e6c3656beca22be808ab270c401dd0bde48b5d
pikokr <parangee9706@gmail.com>
2021-02-11 19:57:00 +0900

    feat: reset with next.js

--- components/ContentSpacer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <div class="h-16 w-full"></div>
-    <div class="container mx-auto px-4 sm:px-0">
-      <slot></slot>
-    </div>
-  </div>
-</template>
--- plugins/fullpage.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import 'fullpage.js/vendors/scrolloverflow'
-import Fullpage from 'vue-fullpage.js'
-import 'fullpage.js/dist/fullpage.css'
-import Fragment from 'vue-fragment'
-
-Vue.use(Fullpage)
-Vue.use(Fragment.Plugin)
