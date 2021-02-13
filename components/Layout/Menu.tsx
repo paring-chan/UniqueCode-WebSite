4b2003a64f83be28b0e648b109320c454bf83cd4
pikokr <parangee9706@gmail.com>
2021-02-13 20:33:51 +0900

    feat: change layout of menu

--- components/Layout/Menu.tsx
+++ components/Layout/Menu.tsx
@@ -3,12 +3,15 @@ import React, { Component } from 'react'
 class Menu extends Component<any, any> {
   render() {
     return (
-      <div
-        className="bg-black h-full fixed w-full z-50 text-white flex px-8 text-xl"
-        style={{
-          alignItems: 'center',
-        }}>
-        sidebar
+      <div className="bg-black h-full fixed w-full z-50 text-white flex flex-col text-xl">
+        <div className="w-full h-20" />
+        <div
+          style={{
+            alignItems: 'center',
+          }}
+          className="h-full px-8">
+          test
+        </div>
       </div>
     )
   }
