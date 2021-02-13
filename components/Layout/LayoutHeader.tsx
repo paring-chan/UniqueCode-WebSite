41d47463f0dd39bb81ffba3e36afc90d6150a0e4
pikokr <parangee9706@gmail.com>
2021-02-13 15:10:49 +0900

    feat: menu component

--- components/Layout/LayoutHeader.tsx
+++ components/Layout/LayoutHeader.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
 import styles from '../../styles/layout.module.css'
+import Menu from './Menu'
 
 class LayoutHeader extends Component<
   {
@@ -36,32 +37,37 @@ class LayoutHeader extends Component<
 
   render() {
     return (
-      <div
-        className={`bg-white h-16 fixed border-b bg-opacity-70 z-50 w-full left-0 top-0 transition-all ${
-          styles.header__bg
-        } ${this.state.navBorder ? 'border-gray-300' : 'border-transparent'}`}>
+      <>
         <div
-          className="flex px-4 container mx-auto h-full"
-          style={{ alignItems: 'center' }}>
-          {this.props.fullpage ? (
-            <a
-              href={this.props.fullpage ? '/#sec1' : '/'}
-              className="text-xl font-bold">
-              UniqueCode
-            </a>
-          ) : (
-            <Link href="/">
+          className={`bg-white h-16 fixed border-b bg-opacity-70 z-50 w-full left-0 top-0 transition-all ${
+            styles.header__bg
+          } ${
+            this.state.navBorder ? 'border-gray-300' : 'border-transparent'
+          }`}>
+          <div
+            className="flex px-4 container mx-auto h-full"
+            style={{ alignItems: 'center' }}>
+            {this.props.fullpage ? (
               <a
                 href={this.props.fullpage ? '/#sec1' : '/'}
                 className="text-xl font-bold">
                 UniqueCode
               </a>
-            </Link>
-          )}
-          <div className="flex-grow" />
-          <div>메뉴</div>
+            ) : (
+              <Link href="/">
+                <a
+                  href={this.props.fullpage ? '/#sec1' : '/'}
+                  className="text-xl font-bold">
+                  UniqueCode
+                </a>
+              </Link>
+            )}
+            <div className="flex-grow" />
+            <div>메뉴</div>
+          </div>
         </div>
-      </div>
+        <Menu />
+      </>
     )
   }
 }
---
+++ components/Layout/Menu.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Menu extends Component<any, any> {
+  render() {
+    return <div>sidebar</div>
+  }
+}
+
+export default Menu
