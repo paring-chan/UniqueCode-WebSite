3f662fb72d390cd993823b7ef20d7f84a47080cb
pikokr <parangee9706@gmail.com>
2021-02-13 21:21:16 +0900

    feat: menu item link

--- components/Layout/MenuItem.tsx
+++ components/Layout/MenuItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 const MenuItem: React.FC<{
   items: {
@@ -8,8 +9,18 @@ const MenuItem: React.FC<{
   }[]
   label: string
   link?: string
-}> = ({ items }) => {
-  return <div>item</div>
+}> = ({ items, label, link }) => {
+  return (
+    <div>
+      <div>
+        {link?.startsWith('/') ? (
+          <Link href={link}>{label}</Link>
+        ) : (
+          <a href={link}>{label}</a>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default MenuItem
