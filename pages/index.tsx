7ad41a3503b095a8005a14be296ef158cd114d85
pikokr <parangee9706@gmail.com>
2021-02-15 11:46:57 +0900

    feat: show more button

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,6 +22,9 @@ const Home: React.FC = () => {
                   }}>
                   <div className="text-4xl font-bold">UniqueCode</div>
                   <div className="text-xl">세상을 바꾸는 청소년 팀</div>
+                  <a className="py-2 px-4 border border-black cursor-pointer">
+                    더보기
+                  </a>
                 </div>
               </div>
               <div className="section">SECTION</div>
