84960f184bce81de93ea2569ee9b58a86e6eff26
pikokr <parangee9706@gmail.com>
2021-02-20 10:50:26 +0900

    feat: reset header component

--- components/Layout/LayoutHeader.tsx
+++ components/Layout/LayoutHeader.tsx
@@ -1,90 +1,10 @@
-import React, { Component } from 'react'
-import Link from 'next/link'
-import styles from '../../styles/layout.module.css'
-import Menu from './Menu'
-import { Menu as MenuIcon, Close as CloseIcon } from '@material-ui/icons'
-import AnimatedMenuIcon from '../icons/AnimatedMenuIcon'
+import React from 'react'
 
-class LayoutHeader extends Component<
-  {
-    fullpage?: boolean
-  },
-  any
-> {
-  handleScroll = () => {
-    if (window.pageYOffset > 0) {
-      if (!this.state.navBorder) {
-        this.setState({ navBorder: true })
-      }
-    } else {
-      if (this.state.navBorder) {
-        this.setState({ navBorder: false })
-      }
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll)
-  }
-
-  state = {
-    navBorder: false,
-    menu: false,
-  }
-
-  render() {
-    return (
-      <>
-        <div
-          className={`bg-white h-16 fixed border-b ${
-            this.state.menu ? 'bg-opacity-100' : 'bg-opacity-70 delay-200'
-          } w-full left-0 top-0 ${styles.header__bg} ${
-            this.state.navBorder ? 'border-gray-300' : 'border-transparent'
-          }`}
-          style={{
-            zIndex: 99999,
-          }}>
-          <div
-            className="flex px-4 container mx-auto h-full"
-            style={{ alignItems: 'center' }}>
-            {this.props.fullpage ? (
-              <a
-                onClick={() => this.setState({menu: false})}
-                href={this.props.fullpage ? '/#sec1' : '/'}
-                className="text-xl font-bold">
-                UniqueCode
-              </a>
-            ) : (
-              <Link href="/">
-                <a
-                  onClick={() => this.setState({menu: false})}
-                  href={this.props.fullpage ? '/#sec1' : '/'}
-                  className="text-xl font-bold">
-                  UniqueCode
-                </a>
-              </Link>
-            )}
-            <div className="flex-grow" />
-            <div>
-              <div onClick={() => this.setState({ menu: !this.state.menu })}>
-                {/*{this.state.menu ? (*/}
-                {/*  <CloseIcon fontSize="large" />*/}
-                {/*) : (*/}
-                {/*  <MenuIcon fontSize="large" />*/}
-                {/*)}*/}
-                <AnimatedMenuIcon active={this.state.menu}/>
-              </div>
-            </div>
-          </div>
-        </div>
-        <Menu open={this.state.menu} />
-      </>
-    )
-  }
+const LayoutHeader = () => {
+  return (
+    <div>
+    </div>
+  )
 }
 
 export default LayoutHeader
