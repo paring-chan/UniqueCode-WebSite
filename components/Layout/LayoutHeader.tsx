b9690fc8e212b36ca74dc8450dd2eb94735fedc5
pikokr <parangee9706@gmail.com>
2021-02-20 13:54:17 +0900

    feat: apply container on header

--- components/Layout/LayoutHeader.tsx
+++ components/Layout/LayoutHeader.tsx
@@ -6,7 +6,7 @@ const LayoutHeader = () => {
     <div
       className="bg-header-black fixed top-0 left-0 w-full text-white flex z-50"
       style={{ height: 44, justifyContent: 'center', alignItems: 'center' }}>
-      <div className="w-full flex">
+      <div className="w-full flex container mx-4">
         <MenuItem>UniqueCode</MenuItem>
         <MenuItem>UniqueCode</MenuItem>
         <MenuItem>UniqueCode</MenuItem>
