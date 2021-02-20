eaa9d214c77b17878de8395ab4426bb1fe82d444
pikokr <parangee9706@gmail.com>
2021-02-20 11:02:04 +0900

    feat: fixed header

--- components/Layout/LayoutHeader.tsx
+++ components/Layout/LayoutHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LayoutHeader = () => {
   return (
     <div
-      className="bg-header-black text-white flex"
+      className="bg-header-black fixed top-0 left-0 w-full text-white flex"
       style={{ height: 44, justifyContent: 'center', alignItems: 'center' }}>
       test
     </div>
