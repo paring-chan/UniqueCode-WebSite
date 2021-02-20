e1e1380fcc657dc205f19726fb640c624c94f393
pikokr <parangee9706@gmail.com>
2021-02-20 14:28:16 +0900

    feat: remove styled-components

--- components/Layout/LayoutHeader.tsx
+++ components/Layout/LayoutHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import MenuItem from './MenuItem'
+import Logo from '../../assets/img/logo.png'
 
 const LayoutHeader = () => {
   return (
@@ -7,7 +8,10 @@ const LayoutHeader = () => {
       className="bg-header-black fixed top-0 left-0 w-full text-white flex z-50"
       style={{ height: 44, justifyContent: 'center', alignItems: 'center' }}>
       <div className="w-full flex container">
-        <MenuItem>UniqueCode</MenuItem>
+        <MenuItem>
+          <img src={Logo} alt="logo" />
+          UniqueCode
+        </MenuItem>
         <MenuItem>Projects</MenuItem>
         <MenuItem>Members</MenuItem>
         <MenuItem>Recruit</MenuItem>
