1874b4a01e094d98b01fd8984b50d457c82c96bd
pikokr <parangee9706@gmail.com>
2021-02-21 10:13:45 +0900

    chore: make href field optional

--- components/Layout/MenuItem.tsx
+++ components/Layout/MenuItem.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 
-const MenuItem: React.FC<{ href: string }> = ({ children, href }) => {
-  return (
-    <Link href={href}>
-      <div className="w-1/4 text-center cursor-pointer hover:text-gray-300 transition-all">
-        {children}
-      </div>
-    </Link>
+const MenuItem: React.FC<{ href?: string }> = ({ children, href }) => {
+  const c = (
+    <div className="w-1/4 text-center cursor-pointer hover:text-gray-300 transition-all">
+      {children}
+    </div>
   )
+
+  return href ? <Link href={href}>{c}</Link> : c
 }
 
 export default MenuItem
