08c1d623eb89885328c86adf870055b4bb7a2e0f
pikokr <parangee9706@gmail.com>
2021-02-21 10:13:04 +0900

    feat: link

--- components/Layout/LayoutHeader.tsx
+++ components/Layout/LayoutHeader.tsx
@@ -8,15 +8,15 @@ const LayoutHeader = () => {
       className="bg-header-black fixed top-0 left-0 w-full text-white flex z-50"
       style={{ height: 44, justifyContent: 'center', alignItems: 'center' }}>
       <div className="w-full flex container">
-        <MenuItem>
+        <MenuItem href="/">
           <div className="flex justify-center">
             <img src={Logo} className="h-6 mr-2" alt="logo" />
             UniqueCode
           </div>
         </MenuItem>
-        <MenuItem>Projects</MenuItem>
-        <MenuItem>Members</MenuItem>
-        <MenuItem>Recruit</MenuItem>
+        <MenuItem href="/projects">Projects</MenuItem>
+        <MenuItem href="/members">Members</MenuItem>
+        <MenuItem href="/recruit">Recruit</MenuItem>
       </div>
     </div>
   )
--- components/Layout/MenuItem.tsx
+++ components/Layout/MenuItem.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
-const MenuItem = ({ children }) => {
+const MenuItem: React.FC<{ href: string }> = ({ children, href }) => {
   return (
-    <div className="w-1/4 text-center cursor-pointer hover:text-gray-300 transition-all">
-      {children}
-    </div>
+    <Link href={href}>
+      <div className="w-1/4 text-center cursor-pointer hover:text-gray-300 transition-all">
+        {children}
+      </div>
+    </Link>
   )
 }
 
